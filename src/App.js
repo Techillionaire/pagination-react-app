be0cb57cd761db2f428ee20040c1362a13579287
Kingsley Onyeka Ifeanyi <80105176+Techillionaire@users.noreply.github.com>
2022-04-15 11:45:16 +0100

    setting up env

--- src/App.js
+++ src/App.js
@@ -1,46 +1,59 @@
+import ReactPaginate from 'react-paginate';
 import './App.css';
-import axios from 'axios';
-import { useState, useEffect } from 'react';
-import Posts from './components/Posts';
-import Pagination from './components/Pagination';
+import React, { useState, useEffect } from 'react'
+
+const perPage = 6;
+
+
 
 const App = () => {
 
-  const [ posts, setPosts ]= useState([]);
-  const [ loading, setLoading ] = useState(false);
-  const [ currentPage, setCurrentPage ] = useState(1);
-  const [ postsPerPage] = useState(10);
+  const [ currentPage, setCurrentPage ] = useState(0);
+  const [ data, setData ] = useState([]);
 
-  useEffect(() => {
-    const fetchPosts = async () => {
-      setLoading(true);
-      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
-      setPosts(res.data);
-      setLoading(false);
-    }
+  useEffect(() =>{
+    fetchData();
+  }, [])
 
-    fetchPosts();
-  }, []);
+  const fetchData = () => {
+    fetch('https://jsonplaceholder.typicode.com/photos')
+    .then((res) => res.json())
+    .then((data) => {
+      setData(data);
+    })
+  }
 
-  // Get current posts
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+  const handlePageClick = ({ selected: selectedPage }) => {
+    console.log('selectedPage', selectedPage);
+    setCurrentPage(selectedPage);
+  }
 
-  // Change page
-  const paginate = (pageNumber) => setCurrentPage(pageNumber)
-  
+  const offset = currentPage * perPage;
+
+  const currentPageData = data
+    .slice(offset, offset + perPage)
+    .map(( res, index ) => <img key={index} src={res.thumbnailUrl} alt='' />)
+
+  const pageCount = Math.ceil(data.length / perPage);
+
   return (
-    <div className="container mt-5">
-      <h1 className='text-primary mb-3'>My Blog</h1>
-      <Posts posts={currentPosts} loading={loading} />
-      <Pagination 
-        postsPerPage={postsPerPage} 
-        totalPosts={posts.length} 
-        paginate={paginate}
+    <div className='App'>
+      <h1>Projects</h1>
+      {currentPageData}
+
+      <ReactPaginate 
+        previousLabel={' <- Previous'}
+        nextLabel = {'Next ->'}
+        pageCount={pageCount}
+        onPageChange={handlePageClick}
+        containerClassName={'pagination'}
+        previousLinkClassName={'paginationLink'}
+        nextClassName={'paginationLink'}
+        disabledClassName={'paginationLinkDisabled'}
+        activeClassName={'paginationLinkActive'}
       />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
